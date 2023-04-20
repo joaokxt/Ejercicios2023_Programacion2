5777b6366d3c83885cec5d0dbf3341036b273da0
joaokxt <joaokx.terck@gmail.com>
2023-04-20 16:55:48 -0300

    Ejercicio calendario

--- u3/u3ej1/fecha.cpp
+++ u3/u3ej1/fecha.cpp
@@ -43,3 +43,17 @@ void Fecha::operator++() {
         else
             incrementar(28);
 }
+void Fecha::operator+(int dias){
+    int i;
+    for(i=0;i<dias;i++) {
+        if(mes==1 || mes==3 || mes==5 || mes==7 || mes==8 || mes==10 || mes==12)
+            incrementar(31);
+        else if(mes==4 || mes==6 || mes==9 || mes==11)
+            incrementar(30);
+        else
+        if(anio%4 == 0 && anio%100 != 0 || anio%400 == 0)
+            incrementar(29);
+        else
+            incrementar(28);
+    }
+}
--- u3/u3ej1/main.cpp
+++ u3/u3ej1/main.cpp
@@ -4,5 +4,69 @@
 using namespace std;
 
 int main() {
+    Fecha *f;
+    char ing;
+    int date[3], i, j, n;
+    bool apagado;
+    do{
+        cout<<"Desea ingresar una fecha de inicio? [S/N] >>";
+        cin>>ing;
+        if(ing==83 || ing==115){
+            for(i=0;i<3;i++){
+                system("cls");
+                cout<<"Ingrese la fecha:"<<endl;
+                while(j<i*3){
+                    cout<<" ";
+                    j++;
+                }
+                cout<<"VV"<<endl;
+                switch(i){
+                    case 0:
+                        cout<<"  /  /  "<<endl;
+                        cout<<"DD: ";
+                        cin>>date[i];
+                        break;
+                    case 1:
+                        cout<<date[0]<<"/  /  "<<endl;
+                        cout<<"MM: ";
+                        cin>>date[i];
+                        break;
+                    case 2:
+                        cout<<date[0]<<"/"<<date[1]<<"/  "<<endl;
+                        cout<<"YYYY: ";
+                        cin>>date[i];
+                        break;
+                }
+            }
+            f=new Fecha(date[0], date[1], date[2]);
+        }else if(ing==78 || ing==110){
+            f=new Fecha();
+        }
+    }while(ing!=83 && ing!=115 && ing!=78 && ing!=110);
 
+    while(apagado==false){
+        system("cls");
+        cout<<f->getDia()<<"/"<<f->getMes()<<"/"<<f->getAnio()<<endl;
+        cout<<"Seleccione una accion: "<<endl;
+        cout<<"1. (+1) dia\n2. (+n) dias\n3. Salir\n>>>"<<endl;
+        cin>>n;
+        switch(n){
+            case 1:
+                ++*f;
+                break;
+            case 2:
+                system("cls");
+                cout<<"n=";
+                cin>>n;
+                *f+n;
+                break;
+            case 3:
+                apagado=true;
+                break;
+            default:
+                cout<<"Opcion no existe"<<endl;
+        }
+    }
+    delete f;
+    return 0;
 }
