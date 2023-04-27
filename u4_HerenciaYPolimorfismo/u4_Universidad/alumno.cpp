ca2a5f50bee1fcd0cdda08a7783adf1e0aab0968
joaokxt <joaokx.terck@gmail.com>
2023-04-27 14:52:49 -0300

    Ejercicio TP clases alumnos, docentes, materias

--- u4_HerenciaYPolimorfismo/u4_Universidad/alumno.cpp
+++ u4_HerenciaYPolimorfismo/u4_Universidad/alumno.cpp
@@ -1,5 +1,22 @@
-//
-// Created by joaok on 27/04/2023.
-//
-
 #include "alumno.h"
+#include "materia.h"
+using namespace std;
+
+Alumno::Alumno(int *id, std::string names, std::string surnames, std::string mail, std::string career, int age):Personal(id, names, surnames, mail){
+    carrera=career;
+    edad=age;
+    inscripto=0;
+}
+void Alumno::setCarrera(string career){
+    carrera=career;
+}
+void Alumno::setEdad(int age){
+    edad=age;
+}
+void Alumno::inscripcion(int *code, string name){
+    materias[inscripto]=new Materia(code, name);
+    inscripto++;
+}
+void Alumno::cargarNota(int indice, int nota){
+    materias[indice-1].setNotas(nota);
+}
--- u4_HerenciaYPolimorfismo/u4_Universidad/materia.h
+++ u4_HerenciaYPolimorfismo/u4_Universidad/materia.h
@@ -4,10 +4,18 @@
 
 #ifndef EJERCICIOS2023_PROGRAMACION2_MATERIA_H
 #define EJERCICIOS2023_PROGRAMACION2_MATERIA_H
+#include <string>
 
-
-class materia {
-
+class Materia {
+private:
+    int codigo[8];
+    std::string nombre;
+    int cargadas;
+    int *notas;
+public:
+    Materia(int *code, std::string name);
+    int* getNotas();
+    void setNotas(int nota);
 };
 
 
